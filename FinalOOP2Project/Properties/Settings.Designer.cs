ac132e887eba27a7572b6f6fe4edd9526c799447
Munir Khaliqyar <le2176022@crc-lennox.qc.ca>
2022-11-28 09:09:25 -0500

    Test checked the application

--- FinalOOP2Project/Properties/Settings.Designer.cs
+++ FinalOOP2Project/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace FinalOOP2Project.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.3.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.4.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -31,6 +31,17 @@ namespace FinalOOP2Project.Properties {
         public string Movie_FinalDB_ProjectConnectionString {
             get {
                 return ((string)(this["Movie_FinalDB_ProjectConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Movie_FinalDB" +
+            "_Project.mdf;Integrated Security=True;Connect Timeout=30")]
+        public string Movie_FinalDB_ProjectConnectionString1 {
+            get {
+                return ((string)(this["Movie_FinalDB_ProjectConnectionString1"]));
             }
         }
     }
