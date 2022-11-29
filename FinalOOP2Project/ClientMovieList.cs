ad472945e2f2881ca87ab989dffa55913e94b43d
Munir Khaliqyar <le2176022@crc-lennox.qc.ca>
2022-11-29 10:19:30 -0500

    set up the ClientMovieList.cs

--- FinalOOP2Project/ClientMovieList.cs
+++ FinalOOP2Project/ClientMovieList.cs
@@ -41,5 +41,33 @@ namespace FinalOOP2Project
             //close the form
             this.Close();
         }
+
+        private void moviesBindingSource1BindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.moviesBindingSource1.EndEdit();
+            this.tableAdapterManager1.UpdateAll(this.movie_FinalDB_ProjectDataSet1);
+
+        }
+
+        private void moviesBindingSource1BindingNavigatorSaveItem_Click_1(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.moviesBindingSource1.EndEdit();
+            this.tableAdapterManager1.UpdateAll(this.movie_FinalDB_ProjectDataSet1);
+
+        }
+
+        private void ClientMovieList_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'movie_FinalDB_ProjectDataSet1.Movies' table. You can move, or remove it, as needed.
+            this.moviesTableAdapter1.Fill(this.movie_FinalDB_ProjectDataSet1.Movies);
+
+        }
+
+        private void moviesBindingSource1BindingNavigator_RefreshItems(object sender, EventArgs e)
+        {
+
+        }
     }
 }
