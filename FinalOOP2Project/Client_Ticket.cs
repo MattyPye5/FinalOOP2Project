64ee3d1424fdadd9d23f9c81bc37fb5cc98c216c
2125697 <LE2125697@crc-lennox.qc.ca>
2022-12-02 08:18:39 -0500

    CreateClient_TicketClass

---
+++ FinalOOP2Project/Client_Ticket.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalOOP2Project
+{
+    internal class Client_Ticket
+    {
+        public Client_Ticket() { }
+        public int TicketId { get; set; }
+        public int ETicket { get; set; }
+
+        public string Availability { get; }
+
+
+       /* public string TicketsAvailable()
+        {
+            MovieTheatreDataClassDataContext db= new MovieTheatreDataClassDataContext();
+           
+
+
+
+            return IsAvailable;
+        }*/
+
+
+    }
+   
+}
