d8f09e8c4e3b08dfe1c9d42b867b3f860dc317f9
2125697 <LE2125697@crc-lennox.qc.ca>
2022-12-06 11:27:01 -0500

    modify Screen_Room class

--- FinalOOP2Project/Screen_Room.cs
+++ FinalOOP2Project/Screen_Room.cs
@@ -14,17 +14,10 @@ namespace FinalOOP2Project
        
         //ScreenRoomDataContext sdb = new ScreenRoomDataContext();
         private List<int> roomList = new List<int>();
-        private List<int> srMovieId = new List<int>();
+      
         public Screen_Room() { }
-        private int roomNo;
-        private int movieId;
-        public int ScreenRoomId { get; set; }
-        public int MovieId { get; set; }
-        public int RoomNo { get; set; }
-        public int TotalSeatNo { get; set; }
-        public List<int> RoomIds { get; set; }
-        public List<int> SRMovieID { get; set; }
-
+        
+       
         public List<int> GetRoomNo()
         {
 
@@ -36,21 +29,8 @@ namespace FinalOOP2Project
 
             return roomList;
         }
-        public List<int> GetMovieId()
-        {
-            foreach (ScreenRoom item in db.ScreenRooms)
-            {
-                srMovieId.Add(item.MovieId);
-                MessageBox.Show("screenmovieId", item.MovieId.ToString());
-            }
-
-            return srMovieId;
-        }
-        //  public int GetRoomNoForSelectedMovie()
-        // {
-
-        //    myMovie.
-        //  }
+        
+      
     }
 }
 
