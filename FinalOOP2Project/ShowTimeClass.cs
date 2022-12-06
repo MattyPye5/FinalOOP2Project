a606d578b0fe433df383da8c4becc6d986acb802
2125697 <LE2125697@crc-lennox.qc.ca>
2022-12-06 11:27:44 -0500

    Add ShowTimeClass

---
+++ FinalOOP2Project/ShowTimeClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalOOP2Project
+{
+    class ShowtimeClass
+    {
+        MovieTheatreDataClassDataContext db = new MovieTheatreDataClassDataContext();
+        private List<DateTime> timeList= new List<DateTime>();
+
+        public List<DateTime> GetShowTime()
+        {
+
+            foreach (ShowTime item in db.ShowTimes)
+            {
+                MessageBox.Show(item.Time.ToString());
+                timeList.Add((DateTime)item.Time);
+            }
+
+            return timeList;
+        }
+    }
+}
