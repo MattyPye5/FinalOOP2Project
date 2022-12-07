521639a87f37fc38a1da2937adfdfffc534f1415
MatthewPyeCST <le1770460@crc-lennox.qc.ca>
2022-12-07 09:31:35 -0500

    Re-Added Register Function and Updated DBML

--- FinalOOP2Project/Register.cs
+++ FinalOOP2Project/Register.cs
@@ -10,11 +10,43 @@ using System.Windows.Forms;
 
 namespace FinalOOP2Project
 {
+    /// <summary>
+    /// Client Register Form
+    /// </summary>
     public partial class Register : Form
     {
+        /// <summary>
+        /// Method Holds form 
+        /// </summary>
         public Register()
         {
             InitializeComponent();
+        }
+
+        MovieTheatreDataClassDataContext db = new MovieTheatreDataClassDataContext();
+
+        private void loginRegisterButton_Click(object sender, EventArgs e)
+        {
+                if (this.passRegisterTextBox.Text == string.Empty)
+                {
+                    MessageBox.Show("Please Fill All Fields");
+                }
+                else if (this.userRegisterTextBox.Text == string.Empty)
+                {
+                    MessageBox.Show("Please Fill All Fields");
+                }
+                else
+                {
+                    User newUser = new User
+                    {
+                        Password = this.passRegisterTextBox.Text,
+                        Username = this.userRegisterTextBox.Text,
+
+                    };
+                    db.Users.InsertOnSubmit(newUser);
+                    db.SubmitChanges();
+                    this.Close();
+                }
         }
     }
 }
