1867dad93b85f3328c2bc6d5b2f22e710ef8ca98
Munir Khaliqyar <le2176022@crc-lennox.qc.ca>
2022-12-07 09:13:29 -0500

    Create ManagerShowTimeForm

---
+++ FinalOOP2Project/ManagerShowTimeForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalOOP2Project
+{
+    public partial class ManagerShowTimeForm : Form
+    {
+        public ManagerShowTimeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void showTimeBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.showTimeBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.movie_FinalDB_ProjectDataSet);
+
+        }
+
+        private void ManagerShowTimeForm_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'movie_FinalDB_ProjectDataSet.ShowTime' table. You can move, or remove it, as needed.
+            this.showTimeTableAdapter.Fill(this.movie_FinalDB_ProjectDataSet.ShowTime);
+
+        }
+    }
+}
